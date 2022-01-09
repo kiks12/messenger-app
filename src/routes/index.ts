5573c1f1a5884e850fa344f01ede74c54c32a6fa
Francis James Tolentino <francistolentino1107@gmail.com>
2022-01-09 11:02:28 +0800

    connected users seen

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,12 @@
 // import express from "express";
 import type { Express} from "express";
+import { Server } from "socket.io";
+import { DefaultEventsMap } from "socket.io/dist/typed-events";
 
 
-function indexRouterHandler(app: Express, io:any) {
+function indexRouterHandler(app: Express, io: Server<DefaultEventsMap, DefaultEventsMap, DefaultEventsMap, any>) {
+
+    var users : any[] = [];
 
     app.get("/", (req:any, res) => {
         if (!req.session.user) {
@@ -14,14 +18,28 @@ function indexRouterHandler(app: Express, io:any) {
          });
     })
     
-    io.on('connection', (socket:any) => {
-        console.log('connected');
+    io.on('connection', (socket) => {
+        
+        socket.on("user-connected", (payload: any) => {
+            const newUser = {
+                id: socket.id,
+                name: payload,
+            }
+            users.push(newUser);
+            socket.broadcast.emit("new-user-connected", users);
+            
+        })
+
+        socket.emit("connected", users);
+
         socket.on("chat-to-server", (payload: any) => {
             socket.broadcast.emit("chat-to-client", payload);
         })
         
         socket.on("disconnect", () => {
-            console.log("disconnected");
+            const disconnectedUser = users.filter(user => user.id !== socket.id);
+            users = disconnectedUser;
+            socket.broadcast.emit("user-disconnected", users);
         })
     })
 
